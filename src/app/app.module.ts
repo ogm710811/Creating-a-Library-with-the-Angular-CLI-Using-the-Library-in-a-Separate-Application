d9596e292df963a4f91b42083dc654006816bc4b
ogm710811 <orestes.garcia.martinez@gmail.com>
2018-08-18 19:47:47 -0400

    test app to installing library & using components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { ExampleNg6LibModule } from 'example-ng6-lib';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ExampleNg6LibModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
